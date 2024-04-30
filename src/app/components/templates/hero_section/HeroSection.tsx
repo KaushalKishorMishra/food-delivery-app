b9ac2f66f3659a66397326f4ac6f53e6b838d9d6
KaushalKishorMishra <077bcsit016.kaushal@scst.edu.np>
2024-04-30 07:02:05 +0545

    4/30/2024 recent commit

--- src/app/components/templates/hero_section/HeroSection.tsx
+++ src/app/components/templates/hero_section/HeroSection.tsx
@@ -1,10 +1,33 @@
+"use client"
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+
+// importing gsap
+import gsap from 'gsap';
+
+// importing gsap ScrollTrigger
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+// activating the plugin
+gsap.registerPlugin(ScrollTrigger);
 
 const HeroSection = () => {
+    const ref = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        const el = ref.current;
+        gsap.fromTo(el, { scale: 0 },
+            {
+                scrollTrigger: {
+                    trigger: el,
+                    scrub: 3,
+                },
+                scale: 1.2,
+                duration: 3,
+            })
+    }, [])
     return (
         <div className='w-full font-serif font-bold relative z-0'>
-            <div className='w-full flex justify-center h-full'>
+            <div className='w-full flex justify-center h-full' ref={ref}>
                 <Image src={'/burger-carousel.png'} alt='Burger Image' className='object-contain w-auto pt-36' priority width={650} height={600} />
             </div>
             <div className='md:text-[14rem] text-center absolute -z-[2] leading-snug tracking-tight text-cs-ascent top-0 drop-shadow-2xl'>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="light">
+    <html lang="en" data-theme="dark">
       <head>
       </head>
       <body className={``}>{children}</body>
